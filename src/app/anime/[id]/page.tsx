167f4113263f30ab95a3b881cbf3e5d651b45562
Kyle <kylerossprestado@gmail.com>
2025-10-15 17:05:45 +0800

    update

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -6,6 +6,6 @@ interface PageProps {
 export default async function Page({params} : PageProps){
     const {id} = params;
 
-    const anime = await getPages({variable: id})
+    const anime = await getPages('', parseInt(id));
     return <div>{JSON.stringify(anime)}</div>
 }
